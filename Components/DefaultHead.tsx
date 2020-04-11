c8e8af9448abc1855ebdceff72529c31cb173072
Radiun Huq <radiunh@docudatasoft.com>
2020-04-11 17:57:48 -0400

    add login and sign up buttons to menu

--- Components/DefaultHead.tsx
+++ Components/DefaultHead.tsx
@@ -7,6 +7,7 @@ type DefaultHeadProps = {}
 const DefaultHead: React.FC<DefaultHeadProps> = () => {
     return (
 		<Head>
+			<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
 			<title>DevFit - do u even lift?</title>
 		</Head>
 	)
--- Components/Menu.tsx
+++ Components/Menu.tsx
@@ -1,6 +1,10 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+
 import React from 'react';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
+
+import { Button } from 'semantic-ui-react';
 
 type MenuProps = {}
 
@@ -8,31 +12,34 @@ export const Menu: React.FC<MenuProps> = () => {
 
     const router = useRouter();
 
+    const menuStyles = {
+        parentDiv: {
+            padding: '5px 0',
+            display: 'flex',
+            flexDirection: 'row' as 'row',
+        },
+        LogoDiv: {
+            padding: '0 5px',
+            flex: 1,
+        },
+        actionsDiv:{ 
+            padding: '0 5px',
+            flex: 1,
+            display: 'flex',
+            justifyContent: 'flex-end'
+        }
+    }
+
     return (
-        <div>
-            <Link href='/'>
-                <p>home</p>
-            </Link>
+        <div css={menuStyles.parentDiv}>
+            <div css={menuStyles.LogoDiv}>
+                <p onClick={() => router.push('/')}>Dev Fit</p>
+            </div>
 
-            <Link href='/feed'>
-                <p>feed</p>
-            </Link>
-
-            <Link href='/leaderboard'>
-                <p>leaderboard</p>
-            </Link>
-
-            <Link href='/login'>
-                <p>login</p>
-            </Link>
-
-            <Link href='/profile'>
-                <p>profile</p>
-            </Link>
-            
-            <Link href='/register'>
-                <p>register</p>
-            </Link>
+            <div css={menuStyles.actionsDiv}>
+                <Button onClick={() => router.push('/login')}>Log In</Button>
+                <Button onClick={() => router.push('/register')} primary>Sign Up</Button>
+            </div>
         </div>
     )
 }
