32fd1005047e7078c77720d1b600e4092be906cd
Ben Awad <benawad97@gmail.com>
2020-04-11 18:00:33 -0500

    login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,4 +1,25 @@
-export default (req, res) => {
-  // Get data from your database
-  res.status(200).json(users);
+import { SessionHandler, session } from "../../middleware/session";
+import { createPrismaClient } from "../../utils/createPrismaClient";
+import { compare } from "bcryptjs";
+
+const handler: SessionHandler = async (req, res, { setUserId }) => {
+  const { username, password } = req.body;
+  const prisma = createPrismaClient();
+  const user = await prisma.user.findOne({ where: { username } });
+  if (!user) {
+    res.status(400).json({ error: "could not find user" });
+    return;
+  }
+
+  const isValid = await compare(password, user.password);
+  if (!isValid) {
+    res.status(400).json({ error: "bad password" });
+    return;
+  }
+
+  setUserId(user.id);
+
+  res.status(200).json({ ok: true, user });
 };
+
+export default session(handler);
