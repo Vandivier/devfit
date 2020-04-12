295fd4b6f525d1f92afc5bd49d434f0f62050a13
Radiun Huq <radiunh@docudatasoft.com>
2020-04-11 20:02:40 -0400

    add base leaderboard page and display on index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import { NextPage } from 'next';
 import MainLayout from '../components/MainLayout';
 import { HomePage } from '../components/Home/HomePage';
+import { LeaderboardPage } from '../components/Leaderboard/LeaderboardPage';
 
 type HomeProps = {}
 
 const Home: NextPage<HomeProps> = () => (
 	<MainLayout>
-		<HomePage />
+		{/* <HomePage /> */}
+		<LeaderboardPage />
 	</MainLayout>
 );
   
