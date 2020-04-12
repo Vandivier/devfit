e6a3ff793974ee19fe9393323c5951e4474cf392
Vandivier <john@afterecon.com>
2020-04-11 21:53:06 -0400

    delete component folders
    consolidate and case fix folder wip

--- components/LoggedInUserProvider.js
+++
@@ -1,9 +0,0 @@
-import React, { createContext } from 'react';
-
-export const LoggedInContext = createContext();
-
-export const LoggedInUserProvider = ({ loggedInUser, children }) => (
-  <LoggedInContext.Provider value={loggedInUser}>
-    {children}
-  </LoggedInContext.Provider>
-);
