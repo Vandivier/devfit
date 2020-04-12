b211fc7e0c0c48fd523dd50c1c93ccea4c75f523
Radiun Huq <radiunh@docudatasoft.com>
2020-04-12 06:43:42 -0400

    clean up repo

--- components/Feed/FeedPage.tsx
+++ components/Feed/FeedPage.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { CloudinaryUpload } from '../../real-components/CloudinaryUpload';
+import { Post } from '@prisma/client';
 
-type FeedPageProps = {};
+type FeedPageProps = {
+    data: Post[];
+};
 
-export const FeedPage: React.FC<FeedPageProps> = () => {
+export const FeedPage: React.FC<FeedPageProps> = ({ data }) => {
     return (
         <div>
             <h1>Feed Page</h1>
+            <NewChallenge />
+            <CloudinaryUpload />
+            <div>{JSON.stringify(data)}</div>
         </div>
     );
 };
+
+type NewChallengeProps = {}
+
+const NewChallenge: React.FC<NewChallengeProps> = () => {
+    return (
+        <div>
+            <p>Challenge</p>      
+        </div>
+    );
+}
