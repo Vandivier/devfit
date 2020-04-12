8354a60a8d6a9d70a39f66d1fbb7a4c3e3118ed9
Radiun Huq <radiunh@docudatasoft.com>
2020-04-12 11:51:51 -0400

    add mobile hamburger menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 import { useAuthenticated } from './useAuthenticated';
@@ -41,7 +41,7 @@ export const Menu: React.FC<MenuProps> = () => {
     const { isAuthenticated, userDetails } = useAuthenticated();
 
     return (
-        <div css={menuStyles.parentDiv}>
+        <div css={menuStyles.parentDiv} id='menuDiv'>
             <div css={menuStyles.LogoDiv}>
                 <div style={{ display: 'flex', height: '100%', alignItems: 'center' }}>
                     <div css={menuStyles.LogoChildDiv} onClick={() => router.push('/')}>
@@ -71,15 +71,96 @@ const UnAuthenticatedMenu: React.FC<UnAuthenticatedMenuProps> = () => {
     );
 };
 
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height
+    };
+  }
+
 type AuthenticatedMenuProps = {};
 
 const AuthenticatedMenu: React.FC<AuthenticatedMenuProps> = () => {
     const router = useRouter();
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+    const [openMobileMenu, setOpenMobileMenu] = useState<boolean>(false);
+    
+    useEffect(() => {
+        function handleResize() {
+          setWindowDimensions(getWindowDimensions());
+        }
+    
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+      }, []);
+
+    const authMenuStyles = {
+        parentDiv: {
+            padding: '10px 0',
+            display: 'flex',
+            flexDirection: 'row' as 'row',
+        },
+        LogoDiv: {
+            padding: '0 5px',
+            flex: 1,
+        },
+        actionsDiv: {
+            padding: '0 5px',
+            flex: 1,
+            display: 'flex',
+            justifyContent: 'flex-end',
+        },
+        LogoChildDiv: {
+            display: 'flex',
+            alignItems: 'center',
+            '&:hover': {
+                opacity: 0.7,
+                cursor: 'pointer',
+            },
+        },
+    };
+
+    const authMenuMobileStyles = {
+        hamburgerDiv: {
+            padding: '0 5px',
+            flex: 1,
+            display: 'flex',
+            justifyContent: 'flex-end',
+        },
+        menuItemsDiv: {
+            height: openMobileMenu ? '100%' : '0%',
+            width: '100%',
+            position: 'absolute' as 'absolute',
+            // top: document.getElementById('menuDiv').style.height,
+            zIndex: 3,
+            display: 'flex',
+            flexDirection: 'column' as 'column',
+            backgroundColor: 'white'
+        }
+    }
 
     return (
-        <div css={menuStyles.actionsDiv}>
-            <Button basic onClick={() => router.push('/leaderboard')}>Leaderboard</Button>
-            <Button secondary onClick={() => router.push('/logout')}>Sign Out</Button>
-        </div>
+        windowDimensions.width > 800 ?
+            <div css={authMenuStyles.actionsDiv}>
+                <Button basic onClick={() => router.push('/profile')}>Profile</Button>
+                <Button basic onClick={() => router.push('/feed')}>Feed</Button>
+                <Button basic onClick={() => router.push('/leaderboard')}>Leaderboard</Button>
+                <Button secondary onClick={() => router.push('/logout')}>Sign Out</Button>
+            </div>
+        :   
+        
+            <div css={authMenuMobileStyles.hamburgerDiv} onClick={() => setOpenMobileMenu(!openMobileMenu)}>
+                <Icon name='bars' size='big' style={{visibility: openMobileMenu ? 'hidden' : 'visible'}} />
+                {openMobileMenu &&
+                    <div css={authMenuMobileStyles.menuItemsDiv}>
+                        <Icon name='bars' size='big' style={{visibility: openMobileMenu ? 'visible' : 'hidden', alignSelf: 'flex-end', marginBottom: 10}} />
+                        <Button basic onClick={() => router.push('/profile')}>Profile</Button>
+                        <Button basic onClick={() => router.push('/feed')}>Feed</Button>
+                        <Button basic onClick={() => router.push('/leaderboard')}>Leaderboard</Button>
+                        <Button secondary onClick={() => router.push('/logout')}>Sign Out</Button>
+                    </div>
+                }
+            </div>
     );
 };
